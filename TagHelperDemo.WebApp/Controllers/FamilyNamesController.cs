4e13125c20c2e12afa6d4fd068b275741d53bf6e
MA <marek@adameczek.pl>
2023-02-13 12:09:20 +0100

    Add project files.

---
+++ TagHelperDemo.WebApp/Controllers/FamilyNamesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TagHelperDemo.WebApp.Models;
+
+namespace TagHelperDemo.WebApp.Controllers;
+
+public class FamilyNamesController : Controller
+{
+    private readonly ILogger<FamilyNamesController> _logger;
+
+    public FamilyNamesController(ILogger<FamilyNamesController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
