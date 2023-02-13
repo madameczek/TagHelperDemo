69fd0cbbafd8967d8534be4818b7c56ee226123a
MA <marek@adameczek.pl>
2023-02-13 14:35:26 +0100

    Add entity.

---
+++ TagHelperDemo.WebApp/Domain/FamilyName.cs
@@ -0,0 +1,9 @@
+﻿namespace TagHelperDemo.WebApp.Domain;
+
+public class FamilyName
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public char Gender { get; set; }
+    public int OccurenceCount { get; set; }
+}
